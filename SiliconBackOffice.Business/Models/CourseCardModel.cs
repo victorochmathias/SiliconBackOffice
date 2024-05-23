cbf5bb7860cdf65af2ccd27d3f30c7274b0bd862
Mathias Rönnmark <mathiasronnmark@hotmail.com>
2024-05-23 14:30:27 +0200

    vet inte vad jag håller på med

---
+++ SiliconBackOffice.Business/Models/CourseCardModel.cs
@@ -0,0 +1,7 @@
+﻿namespace SiliconBackOffice.Business.Models;
+
+public class CourseCardModel
+{
+    public string? Id { get; set; }
+    public string? Title { get; set; }
+}
---
+++ SiliconBackOffice.Business/Models/DynamicGraphQLResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace SiliconBackOffice.Business.Models;
+
+public class DynamicGraphQLResponse
+{
+    [JsonPropertyName("data")]
+    public JsonElement Data { get; set; }
+}
